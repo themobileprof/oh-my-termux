b30b61edb9f1a17fd4a226dd1371bc3d6e098829
TheMobileProf <55714979+themobileprof@users.noreply.github.com>
2024-11-12 12:28:06 +0100

    Update install.sh
    correct error with git clone

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/data/data/com.termux/files/usr/bin/bash
 
-pkg install -y git zsh curl
+pkg install -y git zsh curl zip gh openssh
 clear
 
 if [ -d "$HOME/.termux" ]; then
@@ -10,7 +10,7 @@ fi
 curl -fsLo $HOME/.termux/colors.properties --create-dirs https://cdn.jsdelivr.net/gh/4679/oh-my-termux@master/.termux/colors.properties
 curl -fsLo $HOME/.termux/font.ttf --create-dirs https://cdn.jsdelivr.net/gh/4679/oh-my-termux@master/.termux/font.ttf
 
-git clone git://github.com/robbyrussell/oh-my-zsh.git $HOME/.oh-my-zsh --depth 1
+git clone https://github.com/robbyrussell/oh-my-zsh.git $HOME/.oh-my-zsh --depth 1
 cp $HOME/.oh-my-zsh/templates/zshrc.zsh-template $HOME/.zshrc
 sed -i 's/ZSH_THEME="robbyrussell"/ZSH_THEME="agnoster"/' $HOME/.zshrc
 chsh -s zsh
